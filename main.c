ba1e0b90ee3f792f56fafb71f70b615b723e87cc
Anton Blomberg <anton.blomberg@klarna.com>
2018-10-16 16:45:33 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// https://github.com/ruslo/polly/issues/153
+// sudo apt-get install mingw-w64 g++-mingw-w64
+// sudo apt install wine-stable
+// IntelliJ cmake options: -DCMAKE_TOOLCHAIN_FILE=toolchain-mingw64.cmake
+// wine cmake-build-debug/ctest1.exe
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
